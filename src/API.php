65b330fe8d15d1b2370dd6178ce9a5d9c8fbba1e
jimmyjetter27 <jimmyjetter27@gmail.com>
2022-08-18 11:19:23 +0000

    added send sms

--- src/API.php
+++ src/API.php
@@ -3,23 +3,21 @@
 namespace KorbaXchange;
 
 use Illuminate\Support\Facades\Http;
-use Korba\Util;
 
 class API
 {
     protected function hitting_exchange($endpoint, $payload, $method_type, $authorization, $timeout = 0, $connection_timeout = 300)
     {
-        if (empty(env('EXCHANGE_URL')))
-        {
+        if (empty(env('EXCHANGE_URL'))) {
             return ['success' => false, 'message' => 'Exchange URL is not set'];
         }
 
-        $url = env('EXCHANGE_URL').'/'.$endpoint;
+        $url = env('EXCHANGE_URL') . '/' . $endpoint;
         $proxy_url = env('EXCHANGE_PROXY_URL');
         $payload = array_merge($payload, ['client_id' => env('EXCHANGE_DEFAULT_CLIENT_ID')]);
 
         $res = Http::withHeaders([
-            'Authorization' => 'HMAC '.$authorization,
+            'Authorization' => 'HMAC ' . $authorization,
             'Content-Type' => 'application/json'
         ])
             ->withOptions([
@@ -31,5 +29,34 @@ class API
             ->connectTimeout($connection_timeout)
             ->$method_type($url, $payload);
         return json_decode($res, true);
+    }
+
+
+
+
+    protected function sendSms($message, $phoneNumber)
+    {
+
+        if (empty(env('SMS_AUTH_TOKEN'))) {
+            return ['success' => false, 'message' => 'SMS Auth Token is not set'];
+        } elseif (empty(env('SMS_BASE_URL'))) {
+            return ['success' => false, 'message' => 'SMS Base URL is not set'];
+        }
+
+
+        $changeNumberFormat = KorbaHelper::numberIntFormat($phoneNumber);
+        return $response = Http::withHeaders([
+            'Authorization' => 'Token ' . env('SMS_AUTH_TOKEN'),
+            'Content-Type' => 'application/json'
+        ])
+            ->withOptions([
+                'debug' => fopen('php://stderr', 'w'),
+                'verify' => false
+            ])
+            ->post(env('SMS_BASE_URL'), [
+                'phone_number' => $changeNumberFormat,
+                'message' => $message,
+                'sender_id' => 'VSsms'
+            ]);
     }
 }
