57bd4993bcf13c599f7cefae6e6309d69f713638
jimmyjetter27 <jimmyjetter27@gmail.com>
2022-08-19 11:50:52 +0000

    network lookup added

--- src/API.php
+++ src/API.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 class API
 {
-    protected function hitting_exchange($endpoint, $payload, $method_type, $authorization, $timeout = 0, $connection_timeout = 300)
+    protected function hitting_exchange($endpoint, $payload, $request_type, $authentication_code, $timeout = 0, $connection_timeout = 300)
     {
         if (empty(env('EXCHANGE_URL'))) {
             return ['success' => false, 'message' => 'Exchange URL is not set'];
@@ -17,26 +17,35 @@ class API
         $payload = array_merge($payload, ['client_id' => env('EXCHANGE_DEFAULT_CLIENT_ID')]);
 
         $res = Http::withHeaders([
-            'Authorization' => 'HMAC ' . $authorization,
+            'Authorization' => 'HMAC ' . $authentication_code,
             'Content-Type' => 'application/json'
         ])
             ->withOptions([
-                // add proxy if proxy is set
+                // add proxy if proxy url is set
                 'proxy' => $proxy_url ?: null
-
             ])
             ->timeout($timeout)
             ->connectTimeout($connection_timeout)
-            ->$method_type($url, $payload);
+            ->$request_type($url, $payload);
         return json_decode($res, true);
     }
 
 
+    public function networkLookup($phoneNumber)
+    {
+        if (empty(env('EPESEWA_BASE_URL'))) {
+            return ['success' => false, 'message' => 'EPESEWA BASE URL is not set'];
+        }
 
+        $endpoint = env('EPESEWA_BASE_URL') . '/korba/networklookup/'.$phoneNumber;
+        return Http::withHeaders([
+            'Content-Type' => 'application/json'
+        ])
+        ->post($endpoint);
+    }
 
     protected function sendSms($message, $phoneNumber)
     {
-
         if (empty(env('SMS_AUTH_TOKEN'))) {
             return ['success' => false, 'message' => 'SMS Auth Token is not set'];
         } elseif (empty(env('SMS_BASE_URL'))) {
@@ -45,9 +54,7 @@ class API
             return ['success' => false, 'message' => 'SMS Sender ID is not set'];
         }
 
-
-        $changeNumberFormat = KorbaHelper::numberIntFormat($phoneNumber);
-        return $response = Http::withHeaders([
+        return Http::withHeaders([
             'Authorization' => 'Token ' . env('SMS_AUTH_TOKEN'),
             'Content-Type' => 'application/json'
         ])
@@ -56,7 +63,7 @@ class API
                 'verify' => false
             ])
             ->post(env('SMS_BASE_URL'), [
-                'phone_number' => $changeNumberFormat,
+                'phone_number' => KorbaHelper::numberIntFormat($phoneNumber),
                 'message' => $message,
                 'sender_id' => env('SMS_SENDER_ID')
             ]);
